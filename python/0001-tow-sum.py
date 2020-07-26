74b904a25b3582c7da7205ba8838c03e59b73073
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-26 19:26:12 +0900

    add tow-sum

---
+++ python/0001-tow-sum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+if __name__ == '__main__': 
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], 9))
