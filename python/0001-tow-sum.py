2ff21f97afab8c0dabf28472dfd05034a1d0e2c9
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-26 19:36:46 +0900

    fix

--- python/0001-tow-sum.py
+++ python/0001-tow-sum.py
@@ -5,11 +5,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums) - 1):
+        for i, num in enumerate(nums):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        return []
 
 if __name__ == '__main__': 
     s = Solution()
